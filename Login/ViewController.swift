4ca54fa441aeaba7d505adac33b20529414d954e
Junior <duartejusanca@gmail.com>
2022-01-20 14:07:15 +0100

    first commit

--- Login/ViewController.swift
+++ Login/ViewController.swift
@@ -8,12 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var senButton: UIButton!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passBtn: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("je suis ici")
+        
+      //  send()
+      //  let _ = getName()
+    }
+/*
+    override func viewWillAppear(_ animated: Bool) {
+        
+        super.viewWillAppear(animated)
     }
 
-
+    func send()
+    {
+        print("envoyer")
+    }
+    
+    func getName() -> String?
+    {
+        return nil
+    }
+    */
+    
+    @IBAction func send(_ sender: Any) {
+        let alert = UIAlertController(title: "Attention", message: "Bienvenu", preferredStyle: .alert)
+         UIAlertAction(title: "Fermer", style: .default, handler: nil)
+        
+        let cancel = UIAlertAction(title: "Annuler", style: .cancel, handler: nil)
+        alert.addAction(cancel)
+        
+        self.present(alert, animated: true, completion: nil)
+        //print("Allô Irfane")
+        if emailTextField.text?.count != 0 && passBtn.text?.count != 0 {
+            print("good")
+        }
+    }
+    
 }
 
